d1ea4a1c6d7d7906072c84d5fd47d7c4f7092296
Le Tran Anh Vu <vule5726@gmail.com>
2022-09-20 00:12:05 +0300

    Handle validation exception

--- src/Shop.Api/Filters/ExceptionFilter.cs
+++ src/Shop.Api/Filters/ExceptionFilter.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Shop.Application.Common.Exceptions;
@@ -10,9 +11,16 @@ public class ExceptionFilter : IExceptionFilter
     {
         if (context.Exception is BadRequest)
         {
-            context.Result = new BadRequestObjectResult(new ErrorResponse(StatusCodes.Status400BadRequest, context.Exception.Message));
+            context.Result = new BadRequestObjectResult(new ErrorResponse(StatusCodes.Status400BadRequest, new List<string>{context.Exception.Message}));
+        }
+
+        if (context.Exception is ValidationException)
+        {
+            var ex = (ValidationException)context.Exception;
+            var messages = ex.Errors.Select(v => v.ErrorMessage).ToList();
+            context.Result = new BadRequestObjectResult(new ErrorResponse(StatusCodes.Status400BadRequest, messages));
         }
     }
 }
 
-public record ErrorResponse(int StatusCode, string Message);
+public record ErrorResponse(int StatusCode, IList<string> Messages);
